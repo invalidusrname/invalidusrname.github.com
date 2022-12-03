2b5419fd1b523a5734aec3bb98488183ca890289
Matt McMahand <matt@invalid8.com>
2022-12-03 13:25:00 -0500

    use puma. patch rack lint errors

--- config.ru
+++ config.ru
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 require 'rack/jekyll'
+require_relative 'lib/patches'
 
 run Rack::Jekyll.new
---
+++ lib/patches.rb
@@ -0,0 +1,36 @@
+module Rack
+  class Jekyll
+    def call(env)
+      request = Rack::Request.new(env)
+
+      return serve_wait_page(request) unless complete?
+
+      filename = @files.get_filename(request.path_info)
+
+      if filename
+        media_type = Utils.media_type(filename)
+
+        file = Utils.file_info(filename)
+        body = file[:body]
+        time = file[:time]
+        hdrs = { 'last-modified' => time }
+
+        if time == request.env['HTTP_IF_MODIFIED_SINCE']
+          response = [304, hdrs, []]
+        else
+          hdrs.update({ 'content-length' => body.bytesize.to_s,
+                        'content-type' => media_type })
+          response = [200, hdrs, [body]]
+        end
+
+      else
+        body = not_found_message
+        hdrs = { 'content-length' => body.bytesize.to_s,
+                 'content-type' => 'text/html' }
+        response = [404, hdrs, [body]]
+      end
+
+      request.head? ? remove_body(response) : response
+    end
+  end
+end
